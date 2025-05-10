69ba8a1d73648fb9d00c43313786fe6809e7ed5a
Qhammist <dsneirab@udistrital.edu.co>
2025-05-10 13:48:30 -0500

    Create MergeController.java
    Clase Merge controller

---
+++ src/controller/MergeController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import model.Politico;
+import model.algoritmos.MergeSort;
+import model.estructuras.*;
+import view.VentanaMerge;
+
+public class MergeController {
+    private final VentanaMerge vista;
+
+    public MergeController(VentanaMerge vista) {
+        this.vista = vista;
+    }
+
+    public void ejecutar(int n) {
+        // 1) Generar lista de políticos aleatorios
+        Lista<Politico>[] listas = new Lista[] {
+            new ListaSimple<>(),
+            new ListaDoble<>(),
+            new ListaCircularSimple<>(),
+            new ListaCircularDoble<>()
+        };
+        String[] tipos = {"Simple", "Doble", "Circular Simple", "Circular Doble"};
+
+        for (int i = 0; i < listas.length; i++) {
+            // Llenar
+            for (int j = 0; j < n; j++) {
+                listas[i].add(Politico.aleatorio());
+            }
+            // Medir y ordenar
+            MergeSort.Metricas m = new MergeSort.Metricas();
+            MergeSort.sort(listas[i], m);
+            vista.mostrarResultados(
+                tipos[i] + ": tiempo=" + m.tiempoMillis + "ms  comps=" + m.comparaciones +
+                " swaps=" + m.swaps
+            );
+        }
+    }
+}
